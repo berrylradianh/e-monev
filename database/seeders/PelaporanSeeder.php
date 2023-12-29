3951c22cc79baf738343329694f05e1b97356cac
berrylradianh <berrylhamaesha@gmail.com>
2023-12-29 16:03:07 +0700

    refactor seeder

--- database/seeders/PelaporanSeeder.php
+++ database/seeders/PelaporanSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Pelaporan;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class PelaporanSeeder extends Seeder
@@ -13,40 +12,16 @@ class PelaporanSeeder extends Seeder
      */
     public function run(): void
     {
-        //
-        Pelaporan::create([
-            'no_rm' => '123456',
-            'form1' => false,
-            'form2' => false,
-            'form3' => false,
-        ]);
+        $pelaporans = [
+            ['no_rm' => '123456', 'form1' => false, 'form2' => false, 'form3' => false],
+            ['no_rm' => '012973', 'form1' => false, 'form2' => false, 'form3' => false],
+            ['no_rm' => '102946', 'form1' => false, 'form2' => false, 'form3' => false],
+            ['no_rm' => '126740', 'form1' => false, 'form2' => false, 'form3' => false],
+            ['no_rm' => '126741', 'form1' => false, 'form2' => false, 'form3' => false],
+        ];
 
-        Pelaporan::create([
-            'no_rm' => '012973',
-            'form1' => false,
-            'form2' => false,
-            'form3' => false,
-        ]);
-
-        Pelaporan::create([
-            'no_rm' => '102946',
-            'form1' => false,
-            'form2' => false,
-            'form3' => false,
-        ]);
-
-        Pelaporan::create([
-            'no_rm' => '126740',
-            'form1' => false,
-            'form2' => false,
-            'form3' => false,
-        ]);
-
-        Pelaporan::create([
-            'no_rm' => '126741',
-            'form1' => false,
-            'form2' => false,
-            'form3' => false,
-        ]);
+        foreach ($pelaporans as $pelaporan) {
+            Pelaporan::create($pelaporan);
+        }
     }
 }
--- database/seeders/QuestionSeeder.php
+++ database/seeders/QuestionSeeder.php
@@ -13,52 +13,27 @@ class QuestionSeeder extends Seeder
      */
     public function run(): void
     {
-        //
-        Question::create([
-            'question' => 'Apakah RS Petrokimia Gresik mendukung pelaksanaan ERM ?',
-        ]);
+        $questions = [
+            "Apakah RS Petrokimia Gresik mendukung pelaksanaan ERM ?",
+            "Apakah dengan adanya ERM dapat membantu petugas dalam penyediaan rekam medis pelayanan rawat jalan ?",
+            "Apakah fitur-fitur ERM sudah sesuai kebutuhan untuk ke efektifan kerja petugas dalam menginput data pasien ?",
+            "Apakah ERM memudahkan petugas untuk disiplin terhadap pengisian assesmen awal pasien rawat jalan ?",
+            "Apakah ERM dapat menganalisis data terkait pelaporan mutu pelayanan rawat jalan?",
+            "Apakan penerapan ERM sudah sesuai ketentuan PERMENKES No 24 tahun 2022 ?",
+            "Apakah menu-menu ERM sudah memenuhi indikator-indikator standar mutu pelayanan rumah sakit ?",
+            "Apakah ERM dapat membantu menyelesaikan masalah terkait indikator mutu pelayanan RJ yang sesuai SOP?",
+            "Apakah ERM dapat menjadi salah satu acuan dalam monitoring kinerja petugas?",
+            "Apakah perlu dilakukan evaluasi berkala untuk melihat masalah serta solusi terhadap pemanfaatan ERM di RS Petrokimia Gresik ?",
+            "Apakah kebijakan dalam penggunaan ERM mempertimbangkan latar belakang pendidikan pengguna ERM ?",
+            "Apakah pengimplementasin ERM mudah digunakan oleh petugas?",
+            "Apakah perlu dilakukan pelatihan atau sosialisasi terhadap pemakaian ERM saat diberlakukannya paperless ?",
+            "Apakah terdapat perbedaan hak akses petugas yang hendak melakukan pengisian pada ERM?",
+        ];
 
-        Question::create([
-            'question' => 'Apakah dengan adanya ERM dapat membantu petugas dalam penyediaan rekam medis pelayanan rawat jalan ?',
-        ]);
-        Question::create([
-            'question' => 'Apakah fitur-fitur ERM sudah sesuai kebutuhan untuk ke efektifan kerja petugas dalam menginput data pasien ?',
-        ]);
-        Question::create([
-            'question' => 'Apakah ERM memudahkan petugas untuk disiplin terhadap pengisian assesmen awal pasien rawat jalan ?',
-        ]);
-        Question::create([
-            'question' => 'Apakah ERM memudahkan petugas untuk disiplin terhadap pengisian assesmen awal pasien rawat jalan ?',
-        ]);
-        Question::create([
-            'question' => 'Apakan penerapan ERM sudah sesuai ketentuan PERMENKES No 24 tahun 2022 ?',
-        ]);
-        Question::create([
-            'question' => 'Apakah ERM dapat menganalisis data terkait pelaporan mutu pelayanan rawat jalan ?',
-        ]);
-        Question::create([
-            'question' => 'Apakah menu-menu ERM sudah memenuhi indikator-indikator standar mutu pelayanan rumah sakit ?',
-        ]);
-        Question::create([
-            'question' => 'Apakah ERM dapat membantu menyelesaikan masalah terkait indikator mutu pelayanan RJ yang sesuai SOP ?',
-        ]);
-        Question::create([
-            'question' => 'Apakah ERM dapat menjadi salah satu acuan dalam monitoring kinerja petugas ?',
-        ]);
-        Question::create([
-            'question' => 'Apakah perlu dilakukan evaluasi berkala untuk melihat masalah serta solusi terhadap pemanfaatan ERM di RS Petrokimia Gresik ?',
-        ]);
-        Question::create([
-            'question' => 'Apakah kebijakan dalam penggunaan ERM mempertimbangkan latar belakang pendidikan pengguna ERM ?',
-        ]);
-        Question::create([
-            'question' => 'Apakah pengimplementasin ERM mudah digunakan oleh petugas ?',
-        ]);
-        Question::create([
-            'question' => 'Apakah perlu dilakukan pelatihan atau sosialisasi terhadap pemakaian ERM saat diberlakukannya paperless ?',
-        ]);
-        Question::create([
-            'question' => 'Apakah terdapat perbedaan hak akses petugas yang hendak melakukan pengisian pada ERM ?',
-        ]);
+        foreach ($questions as $question) {
+            Question::create([
+                'question' => $question,
+            ]);
+        }
     }
 }
